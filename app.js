b2d47c08c4ad2fadf233b4c66c15ab858ef67d94
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-04-26 23:04:46 -0700

    add check for if location is in United States

--- app.js
+++ app.js
@@ -10,16 +10,6 @@ function getCity() {
 
 let imperial = true;
 
-function changeUnits() {
-  if (imperial === true) {
-    imperial = false;
-    unitDisplay();
-  } else {
-    imperial = true;
-    unitDisplay();
-  }
-}
-
 function unitDisplay() {
   const impSpan = document.getElementById('degreesF');
   const metSpan = document.getElementById('degreesC');
@@ -33,22 +23,57 @@ function unitDisplay() {
 }
 
 const cityNameEl = document.getElementById('city-name');
+const cityStateEl = document.getElementById('city-state');
 const humidityEl = document.getElementById('humidity');
 const windSpeedEl = document.getElementById('wind-speed');
+const dateEl = document.getElementById('current-date');
+const feelsLikeEl = document.getElementById('feels-like');
+const chanceOfRainEl = document.getElementById('chance-of-rain');
 
-function updateUIImperial(data) {}
+function checkIfUnitedStates(data) {
+  let country = data.location.country;
+  let state = data.location.region;
+
+  if (country === 'United States of America') {
+    cityStateEl.textContent = state;
+  } else {
+    cityStateEl.textContent = country;
+  }
+}
+
+function updateUIImperial(data) {
+  windSpeedEl.textContent = data.current.wind_mph + 'mph';
+  feelsLikeEl.textContent = data.current.feelslike_f + 'F°';
+}
 
 function updateUIMetric(data) {
-  cityNameEl.textContent = data.location.name;
-  humidityEl.textContent = data.current.humidity;
-  windSpeedEl.textContent = data.current.wind_kph;
+  windSpeedEl.textContent = data.current.wind_kph + 'km/h';
+  feelsLikeEl.textContent = data.current.feelslike_c + 'C°';
 }
 
 function updateUI(data) {
+  cityNameEl.textContent = data.location.name;
+  humidityEl.textContent = data.current.humidity;
+  chanceOfRainEl.textContent =
+    data.forecast.forecastday[0].day.daily_chance_of_rain;
+
+  checkIfUnitedStates(data);
   if (imperial === true) {
     updateUIImperial(data);
   } else {
     updateUIMetric(data);
+  }
+}
+
+function changeUnits() {
+  if (imperial === true) {
+    imperial = false;
+    unitDisplay();
+    updateUI(data);
+  } else {
+    imperial = true;
+    unitDisplay();
+    updateUI(data);
   }
 }
 
